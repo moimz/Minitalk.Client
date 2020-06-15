92bb2254503f52b2bbe97a38628a13a8f9e0af2f
arzzcom <arzz@arzz.com>
2020-06-16 06:09:27 +0900

    UI 처리 클래스 추가

---
+++ scripts/widget.ui.js
@@ -0,0 +1,13 @@
+/**
+ * 이 파일은 미니톡 클라이언트의 일부입니다. (https://www.minitalk.io)
+ *
+ * 미니톡 채팅위젯 UI 관련 이벤트를 처리한다.
+ * 
+ * @file /scripts/widget.ui.js
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 7.0.0
+ * @modified 2020. 6. 16.
+ */
+Minitalk.ui = {
+};
