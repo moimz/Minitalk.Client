3000e427d92ffce8b25a3d492e4a29f04ac0a28c
arzzcom <arzz@arzz.com>
2020-07-08 10:43:56 +0900

    개인박스 관련 코드 초기화

---
+++ scripts/widget.box.js
@@ -0,0 +1,13 @@
+/**
+ * 이 파일은 미니톡 클라이언트의 일부입니다. (https://www.minitalk.io)
+ *
+ * 미니톡 개인채널 개설/참여와 관련된 클래스를 정의한다.
+ * 
+ * @file /scripts/widget.box.js
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 7.0.0
+ * @modified 2020. 7. 8.
+ */
+Minitalk.box = {
+};
