ee8e63bc3986b7609d54e5132d41cda6927937b5
arzzcom <arzz@arzz.com>
2020-07-08 10:46:57 +0900

    Update box.js

--- scripts/box.js
+++ scripts/box.js
@@ -57,15 +57,19 @@ Minitalk.getErrorText = function(code) {
  * 브라우져의 세션스토리지에 데이터를 저장한다.
  *
  * @param string name 변수명
- * @param any value 저장될 데이터 (없을 경우 저장되어있는 데이터를 리턴한다.)
+ * @param any value 저장될 데이터 (없을 경우 저장되어있는 데이터를 반환한다.)
+ * @return boolean/any 데이터저장성공여부, 저장되어 있는 데이터
  */
 Minitalk.session = function(name,value) {
-	if (window.sessionStorage === undefined) return;
+	if (window.sessionStorage === undefined) {
+		// @todo 개인정보보호모드일 경우 일부 기능이 제한될 수 있다는 안내메시지 출력
+		return;
+	}
 	
 	var storage = {};
-	if (window.sessionStorage["minitalk-" + Minitalk.channel] !== undefined) {
+	if (window.sessionStorage["Minitalk." + Minitalk.id + "." + Minitalk.channel] !== undefined) {
 		try {
-			storage = JSON.parse(window.sessionStorage["minitalk-" + Minitalk.channel]);
+			storage = JSON.parse(window.sessionStorage["Minitalk." + Minitalk.id + "." + Minitalk.channel]);
 		} catch (e) {
 			storage = {};
 		}
@@ -80,7 +84,7 @@ Minitalk.session = function(name,value) {
 	} else {
 		try {
 			storage[name] = value;
-			window.sessionStorage["minitalk-" + Minitalk.channel] = JSON.stringify(storage);
+			window.sessionStorage["Minitalk." + Minitalk.id + "." + Minitalk.channel] = JSON.stringify(storage);
 			
 			return true;
 		} catch (e) {
@@ -93,16 +97,19 @@ Minitalk.session = function(name,value) {
  * 브라우져의 로컬스토리지에 데이터를 저장한다.
  *
  * @param string name 변수명
- * @param any value 저장될 데이터 (없을 경우 저장되어있는 데이터를 리턴한다.)
+ * @param any value 저장될 데이터 (없을 경우 저장되어있는 데이터를 반환한다.)
+ * @return boolean/any 데이터저장성공여부, 저장되어 있는 데이터
  */
 Minitalk.storage = function(name,value) {
-	if (Minitalk.isPrivateChannel == true) return;
-	if (window.localStorage === undefined) return;
+	if (window.localStorage === undefined) {
+		// @todo 개인정보보호모드일 경우 일부 기능이 제한될 수 있다는 안내메시지 출력
+		return;
+	}
 	
 	var storage = {};
-	if (window.localStorage["minitalk-" + Minitalk.channel] !== undefined) {
+	if (window.localStorage["Minitalk." + Minitalk.id + "." + Minitalk.channel] !== undefined) {
 		try {
-			storage = JSON.parse(window.localStorage["minitalk-" + Minitalk.channel]);
+			storage = JSON.parse(window.localStorage["Minitalk." + Minitalk.id + "." + Minitalk.channel]);
 		} catch (e) {
 			storage = {};
 		}
@@ -117,7 +124,7 @@ Minitalk.storage = function(name,value) {
 	} else {
 		try {
 			storage[name] = value;
-			window.localStorage["minitalk-" + Minitalk.channel] = JSON.stringify(storage);
+			window.localStorage["Minitalk." + Minitalk.id + "." + Minitalk.channel] = JSON.stringify(storage);
 			
 			return true;
 		} catch (e) {
@@ -126,5 +133,68 @@ Minitalk.storage = function(name,value) {
 	}
 };
 
+/**
+ * 미니톡 환경설정 데이터를 저장한다.
+ *
+ * @param string name 변수명
+ * @param any value 저장될 데이터 (없을 경우 저장되어 있는 데이터를 반환한다.)
+ * @return boolean/any 데이터저장성공여부, 저장되어 있는 데이터
+ */
+Minitalk.configs = function(name,value) {
+	/**
+	 * 기본설정값
+	 */
+	var defaults = {
+		
+	};
+	
+	var configs = Minitalk.storage("configs") == null ? defaults : Minitalk.storage("configs");
+	if (value === undefined) {
+		if (name === undefined) return configs;
+		if (configs[name] !== undefined) {
+			return configs[name];
+		} else {
+			return null;
+		}
+	} else {
+		configs[name] = value;
+		return Minitalk.storage("configs",configs);
+	}
+};
+
 $(document).ready(function() {
+	/**
+	 * 에러가 발생했다면, 에러코드를 출력한다.
+	 */
+	if ($("body").attr("data-error")) {
+		var $error = $("<div>").attr("data-role","error");
+		var $errorbox = $("<section>");
+		$errorbox.append($("<h2>").html(Minitalk.getText("text/error")));
+		$errorbox.append($("<p>").html(Minitalk.getText("error/"+$("body").attr("data-error"))));
+		$errorbox.append($("<a>").attr("href","https://www.minitalk.io/").attr("target","_blank").html(Minitalk.getText("text/minitalk_homepage")));
+		$error.append($("<div>").append($errorbox));
+		$("body").append($error);
+		return;
+	}
+	
+	/**
+	 * 채팅위젯 클래스를 초기화한다.
+	 */
+	Minitalk.box.init();
+	
+	/**
+	 * 활성화된 플러그인을 초기화한다.
+	 */
+	for (var plugin in Minitalk.plugins) {
+		if (typeof Minitalk.plugins[plugin].init == "function") {
+			Minitalk.plugins[plugin].init();
+		}
+	}
+	
+	/**
+	 * 채팅위젯 템플릿 스타일시트를 새로 불러왔을 경우, 스타일시트에 영향을 받는 요소를 초기화한다.
+	 */
+	$("link[rel=stylesheet]").on("load",function() {
+		Minitalk.ui.initFrame();
+	});
 });
