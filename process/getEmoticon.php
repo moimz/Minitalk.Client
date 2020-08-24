c03134da4272ebd812a2a51ac016c2c4b608abfc
arzzcom <arzz@arzz.com>
2020-08-24 16:04:03 +0900

    서버의 이모티콘 그룹 및 정보를 가져오는 프로세스 추가

---
+++ process/getEmoticon.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * 이 파일은 미니톡 클라이언트의 일부입니다. (https://www.minitalk.io)
+ *
+ * 특정 이모티콘 그룹의 폴더를 읽어, 이모티콘을 가져온다.
+ * 
+ * @file /process/getEmoticon.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 7.0.0
+ * @modified 2020. 8. 23.
+ */
+if (defined('__MINITALK__') == false) exit;
+
+$category = str_replace('/','',Request('category'));
+if (!$category || is_dir(__MINITALK_PATH__.'/emoticons/'.$category) == false || is_dir(__MINITALK_PATH__.'/emoticons/'.$category.'/items') == false) {
+	$results->success = false;
+	$results->error = $this->getErrorText('NOT_FOUND_EMOTICON');
+	return;
+}
+
+$items = array();
+$emoticonsPath = @opendir(__MINITALK_PATH__.'/emoticons/'.$category.'/items');
+while ($emoticonName = @readdir($emoticonsPath)) {
+	if ($emoticonName != '.' && $emoticonName != '..' && is_file(__MINITALK_PATH__.'/emoticons/'.$category.'/items/'.$emoticonName) == true) {
+		$items[] = $category.'/'.$emoticonName;
+	}
+}
+
+$results->success = true;
+$results->items = $items;
+?>
---
+++ process/getEmoticons.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 이 파일은 미니톡 클라이언트의 일부입니다. (https://www.minitalk.io)
+ *
+ * 이모티콘 폴더를 읽어, 이모티콘 그룹을 가져온다.
+ * 
+ * @file /process/getEmoticons.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 7.0.0
+ * @modified 2020. 8. 23.
+ */
+if (defined('__MINITALK__') == false) exit;
+
+$emoticons = array();
+$emoticonsPath = @opendir(__MINITALK_PATH__.'/emoticons');
+while ($emoticonName = @readdir($emoticonsPath)) {
+	if ($emoticonName != '.' && $emoticonName != '..' && is_dir(__MINITALK_PATH__.'/emoticons/'.$emoticonName) == true && is_file(__MINITALK_PATH__.'/emoticons/'.$emoticonName.'/package.json') == true) {
+		$package = json_decode(file_get_contents(__MINITALK_PATH__.'/emoticons/'.$emoticonName.'/package.json'));
+		if ($package == null) continue;
+		
+		$package->category = $emoticonName;
+		$emoticons[] = $package;
+	}
+}
+
+$results->success = true;
+$results->emoticons = $emoticons;
+?>
