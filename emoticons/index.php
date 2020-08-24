e707d9887f1ceb87ee74ae4216be36097022f0b9
arzzcom <arzz@arzz.com>
2020-08-24 16:05:36 +0900

    이모티콘 기본폴더 추가

---
+++ emoticons/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 이 파일은 미니톡 클라이언트의 일부입니다. (https://www.minitalk.io)
+ *
+ * 폴더 접근 제한을 위한 Header 설정
+ * 
+ * @file /emoticons/index.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 7.0.0
+ * @modified 2020. 3. 16.
+ */
+header('HTTP/1.1 404 Not Found');
+?>
