517bfd92885ab5db2b01bad1d4ddd2de67ffdc0d
arzzcom <arzz@arzz.com>
2020-11-10 12:27:48 +0900

    폴더접근 수정

--- plugins/index.php
+++ plugins/index.php
@@ -2,7 +2,7 @@
 /**
  * 이 파일은 미니톡 클라이언트의 일부입니다. (https://www.minitalk.io)
  *
- * 폴더 접근 제한을 위한 Header 설정
+ * 플러그인 폴더 접근을 막기 위한 인덱스파일
  * 
  * @file /plugins/index.php
  * @author Arzz (arzz@arzz.com)
@@ -10,5 +10,6 @@
  * @version 7.0.0
  * @modified 2020. 3. 16.
  */
-header('HTTP/1.1 404 Not Found');
+header("HTTP/1.1 403 Forbidden");
+header('location:../');
 ?>
--- templets/index.php
+++ templets/index.php
@@ -2,14 +2,14 @@
 /**
  * 이 파일은 미니톡 클라이언트의 일부입니다. (https://www.minitalk.io)
  *
- * 이모티콘 폴더 접근을 막기 위한 인덱스파일
+ * 템플릿폴더 접근을 막기 위한 인덱스파일
  * 
- * @file /emoticons/index.php
+ * @file /templets/index.php
  * @author Arzz (arzz@arzz.com)
  * @license MIT License
- * @version 7.0.0
- * @modified 2020. 3. 16.
+ * @version 3.0.0
+ * @modified 2018. 3. 29.
  */
 header("HTTP/1.1 403 Forbidden");
-header('location:../');
+header("location:../");
 ?>
