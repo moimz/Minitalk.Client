a2ed6065baf510e6b49738d05bf8a4c86cbd9e14
arzzcom <arzz@arzz.com>
2020-12-23 19:25:03 +0900

    환경설정 변수 정리

--- scripts/minitalk.js
+++ scripts/minitalk.js
@@ -98,70 +98,112 @@ if (isMinitalkIncluded === undefined) {
 	 */
 	var Minitalk = function(opt) {
 		/**
-		 * 미니톡 채팅위젯 환경설정변수들을 초기화하여 정의한다.
-		 * 상세한 내용은 https://www.minitalk.io/ko/manual/api/widget 페이지를 참고하여 주십시오.
+		 * 미니톡 채팅위젯 환경설정변수들을 초기화하여 정의한다. (필수정보)
+		 * 상세한 내용은 https://www.minitalk.io/ko/manual/api 페이지를 참고하여 주십시오.
 		 *
 		 * @public string id 미니톡 채팅위젯의 고유 ID (없을 경우 랜덤생성)
-		 * @public string templet 미니톡 채팅위젯의 템플릿 (기본값 : default)
 		 * @public string channel 미니톡 채팅위젯의 채널명 (필수)
-		 * @public string usercode 암호화된 접속자정보(닉네임, 레벨, 닉이미지, 사진)
-		 * @public object userdata 암호화되지 않은 접속자 추가정보
+		 * @public string nickname 접속자명
+		 * @public string nickcon 접속자 이미지닉네임 이미지주소
+		 * @public object info 접속자 세부정보 객체
+		 * @public string opperCode 암호화된 접속자 권한코드정보
 		 *
-		 * @public string background 미니톡 채팅위젯 영역의 기본 배경스타일
-		 * @public boolean viewUserMessage 다른 접속자의 접속안내 메시지를 보일지 설정한다. (기본값 : true, true : 접속안내메시지 보임, false : 접속안내메시지 숨김)
-		 * @public int viewUserLimit 접속안내 메시지 및 유저목록에서 보일 유저의 최소레벨 (기본값 : 0)
+		 * @public string title 미니톡이 로딩되는 동안 표시된 위젯제목 (접속시 채널명이 제목으로 사용됩니다.)
+		 * @public string templet 미니톡 채팅위젯의 템플릿 (기본값 : default)
+		 * @public string type 미니톡 채팅위젯 형태(기본값 : 자동, horizontal : 가로모드, vertical : 세로모드, auto : 자동)
+		 * @public string language 언어코드 (기본값 : ko)
+		 * @public int/string width 미니톡 채팅위젯 가로크기 (기본값 : 200, 픽셀 또는 % 단위)
+		 * @public int/string height 미니톡 채팅위젯 세로크기 (기본값 : 600, 픽셀 또는 % 단위)
+		 *
+		 * @public boolean viewUser 접속자목록 표시여부
+		 * @public string viewUserLimit 접속안내 메시지 및 유저목록에서 보일 유저의 최소레벨 (기본값 : ALL)
+		 * @public boolean viewUserNotification 다른 접속자의 접속안내 메시지를 보일지 설정한다. (기본값 : true, true : 접속안내메시지 보임, false : 접속안내메시지 숨김)
+		 * @public string viewUserNotificationLimit 접속안내 메시지 및 유저목록에서 보일 유저의 최소레벨 (기본값 : ALL)
+		 * @public boolean viewStatusIcon 유저목록에 상태아이콘을 보일지 여부 설정
+		 * @public string statusIconPath 상태아이콘 경로
+		 * @public int userListWidth 유저목록의 가로너비 (기본값 : 160, 가로모드에서 적용)
+		 * @public int userListHeight 유저목록의 세로높이 (기본값 : 100, 세로모드에서 적용)
+		 
 		 * @public int logCount 이전 대화내역을 몇개까지 보일지 설정한다. (기본값 : 15, 최대 : 30, 0 일 경우 이전 대화내역을 사용하지 않는다.)
+		 * @public string splitString 대화내용에서 접속자닉네임과 대화내용을 구분할 문자를 설정한다. (기본값 : :)
+		 * @public string dateFormat 미니톡 채팅위젯내에서 표시되는 날짜/시각의 표시형태
 		 *
-		 * @public string/object[] tabs 탭바의 탭을 설정한다.
-		 * @public string tabType 탭바 형식을 정의한다.  (기본값 : auto, auto : 채팅위젯 가로크기(400px 기준)에 따라 가로/세로 자동변환, horizontal : 가로형태, vertical : 세로형태)
+		 * @public string toolType 툴바 형식을 정의한다. (기본값 : icon, icon : 툴바아이콘만 표시, text : 툴바명칭만 표시, icontext : 툴바 아이콘 및 명칭표시)
 		 * @public object[] tools 툴바의 버튼을 정의한다.
 		 * @public object[] usermenus 접속자를 클릭했을 때 보일 메뉴를 설정한다.
-		 *
-		 * @public int/string width 미니톡 채팅위젯 가로크기를 픽셀 또는 % 단위로 설정한다. (기본값 : 200)
-		 * @public int/string height 미니톡 채팅위젯 세로크기를 픽셀 또는 % 단위로 설정한다. (기본값 : 600)
-		 * @public string dateFormat 미니톡 채팅위젯내에서 표시되는 날짜/시각의 표시형태
 		 *
 		 * @public object[] listeners 이벤트리스너를 정의한다.
 		 * @public object[] protocols 사용자정의 프로토콜을 정의한다.
 		 */
 		this.id = opt.id ? opt.id : null;
 		this.channel = opt.channel ? opt.channel : null;
-		this.private = opt.private ? opt.private : null;
 		this.nickname = opt.nickname ? opt.nickname : "";
 		this.nickcon = opt.nickcon ? opt.nickcon : "";
-		this.opperCode = opt.opperCode && opt.opperCode != "" ? opt.opperCode : null;
 		this.info = opt.info && typeof opt.info == "object" ? opt.info : {};
-		this.sns = opt.sns ? opt.sns : "";
-		this.device = opt.device ? opt.device : "PC";
-		this.skin = opt.skin;
-		this.templet = opt.templet ? opt.templet : opt.skin;
-		this.type = opt.type ? opt.type : "auto";
+		this.opperCode = opt.opperCode && opt.opperCode != "" ? opt.opperCode : null;
+		
 		this.title = opt.title ? opt.title : null;
-		this.language = opt.language;
-		this.plugin = opt.plugin ? opt.plugin : "ALL";
+		this.templet = opt.templet ? opt.templet : null;
+		// v6.3.x 대응 (v6.5.0 에서 제거예정)
+		if (opt.skin !== undefined) {
+			console.warn("[deprecated] skin is deprecated in v6.5. use templet.");
+			this.templet = opt.skin;
+		}
+		this.type = opt.type ? opt.type : "auto";
+		this.language = opt.language ? opt.language : "ko";
 		opt.width = opt.width ? opt.width : 200;
 		opt.height = opt.height ? opt.height : 600;
 		this.width = opt.width.toString().indexOf("%") < 0 ? opt.width+"px" : opt.width;
 		this.height = opt.height.toString().indexOf("%") < 0 ? opt.height+"px" : opt.height;
 		
-		this.userListWidth = opt.userListWidth ? opt.userListWidth : 160;
-		this.userListHeight = opt.userListHeight ? opt.userListHeight : 100;
 		this.viewUser = opt.viewUser === false ? false : true;
 		this.viewUserLimit = opt.viewUserLimit ? opt.viewUserLimit : "ALL";
-		this.viewAlert = opt.viewAlert === false ? false : true;
-		this.viewAlertLimit = opt.viewAlertLimit ? opt.viewAlertLimit : "ALL";
+		this.viewUserNotification = opt.viewUserNotification === false ? false : true;
+		// v6.3.x 대응 (v6.5.0 에서 제거예정)
+		if (opt.viewAlert !== undefined) {
+			console.warn("[deprecated] viewAlert is deprecated in v6.5. use viewUserNotification.");
+			this.viewUserNotification = opt.viewAlert;
+		}
+		this.viewUserNotificationLimit = opt.viewUserNotificationLimit ? opt.viewUserNotificationLimit : "ALL";
+		// v6.3.x 대응 (v6.5.0 에서 제거예정)
+		if (opt.viewAlertLimit !== undefined) {
+			console.warn("[deprecated] viewAlertLimit is deprecated in v6.5. use viewUserNotificationLimit.");
+			this.viewUserNotificationLimit = opt.viewAlertLimit;
+		}
 		this.viewStatusIcon = opt.viewStatusIcon === false ? false : true;
+		this.statusIconPath = opt.statusIconPath !== undefined ? opt.statusIconPath : MinitalkComponent.getUrl() + "/images/status";
+		this.userListWidth = opt.userListWidth ? opt.userListWidth : 160;
+		this.userListHeight = opt.userListHeight ? opt.userListHeight : 100;
 		
-		this.statusIconPath = opt.statusIconPath !== undefined ? opt.statusIconPath : "";
+		this.logCount = opt.logCount !== undefined ? opt.logCount : 15;
+		if (opt.logLimit !== undefined) {
+			console.warn("[deprecated] logLimit is deprecated in v6.5. use logCount.");
+			this.logCount = opt.logLimit;
+		}
 		this.splitString = opt.splitString ? opt.splitString : " : ";
+		this.dateFormat = opt.dateFormat ? opt.dateFormat : "A HH:mm";
 		
-		this.addToolList = opt.addToolList ? opt.addToolList : [];
-		this.addUserMenuList = opt.addUserMenuList ? opt.addUserMenuList : [];
 		this.toolType = opt.toolType ? opt.toolType : "icon";
+		this.tools = opt.tools ? opt.tools : ["bold","underline","italic","color","-","emoticon","-","mute","push","-","scroll","-","clear"];
+		// v6.3.x 대응 (v6.5.0 에서 제거예정)
+		if (opt.addToolList !== undefined) {
+			console.warn("[deprecated] addToolList is deprecated in v6.5. use tools.");
+			for (var i=0, loop=opt.addToolList.length;i<loop;i++) {
+				this.tools.push(opt.addToolList[i]);
+			}
+		}
+		this.usermenus = opt.usermenus ? opt.usermenus : ["configs","-","whisper","call","-","create","invite","-","banmsg","showip","banip","opper","deopper"];
+		// v6.3.x 대응 (v6.5.0 에서 제거예정)
+		if (opt.addUserMenuList !== undefined) {
+			console.warn("[deprecated] addUserMenuList is deprecated in v6.5. use usermenus.");
+			for (var i=0, loop=opt.addUserMenuList.length;i<loop;i++) {
+				this.usermenus.push(opt.addUserMenuList[i]);
+			}
+		}
 		
-		this.logLimit = opt.logLimit !== undefined ? opt.logLimit : 15;
-		this.fontSettingLimit = opt.fontSettingLimit ? opt.fontSettingLimit : "ALL";
-		this.fontSettingHide = opt.fontSettingHide === true ? true : false;
+		this.device = opt.device ? opt.device : "PC";
+		this.private = opt.private ? opt.private : null;
+		
 		
 		this.showChannelConnectMessage = opt.showChannelConnectMessage === false ? false : true;
 		
@@ -176,7 +218,6 @@ if (isMinitalkIncluded === undefined) {
 		this.uuid = null;
 		this.maxuser = 0;
 		this.isPrivate = this.private == null ? false : true;
-		this.isAutoHideUserList = false;
 		this.isAlertStorage = false;
 		
 		/**
