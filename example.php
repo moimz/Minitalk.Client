1b3445866e9dd4506d8bb78aa356eccec6b880a1
arzzcom <arzz@arzz.com>
2020-12-24 10:07:08 +0900

    예제코드 변경

--- example.php
+++ example.php
@@ -26,25 +26,22 @@ HTML, BODY {padding:10px; margin:0px; overflow:hidden; width:100%; height:100%;}
 html, body {width:100%; height:100%; padding:0; margin:0;}
 </style>
 <body>
-	<script type="text/javascript" src="./scripts/minitalk.js" charset="UTF-8"></script>
-	<script type="text/javascript">
-	var m = new Minitalk({
-		id:"MinitalkExample",
-		channel:"example",
-		width:"100%",
-		height:"100%",
-		templet:"default",
-		language:"ko",
-		type:"auto",
-		nickcon:"",
-		nickname:"",
-		info:{},
-		opperCode:"<?php echo MinitalkOpperCode('MEMBER'); ?>",
-		fontSettingHide:false,
-		viewAlert:true,
-		viewAlertLimit:"MEMBER",
-		plugin:"ALL"
-	});
-	</script>
+	<div style="width:405px; height:505px; background:red;">
+		<script type="text/javascript" src="./scripts/minitalk.js" charset="UTF-8"></script>
+		<script type="text/javascript">
+		var m = new Minitalk({
+			id:"MinitalkExample",
+			channel:"example",
+			width:"100%",
+			height:"100%",
+			templet:"default",
+			language:"ko",
+			type:"auto",
+			nickname:"손님(<?php echo rand(10000,99999); ?>)",
+			opperCode:"<?php echo MinitalkOpperCode('MEMBER'); ?>",
+			toolType:"icontext"
+		});
+		</script>
+	</div>
 </body>
 </html>
