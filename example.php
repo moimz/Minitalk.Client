57db4714b41d476e2911ceaa6518c33909bf48cb
arzzcom <arzz@arzz.com>
2020-12-28 15:02:24 +0900

    id 변경

--- example.php
+++ example.php
@@ -29,7 +29,7 @@ html, body {width:100%; height:100%; padding:0; margin:0;}
 	<script type="text/javascript" src="./scripts/minitalk.js" charset="UTF-8"></script>
 	<script type="text/javascript">
 	var m = new Minitalk({
-		id:"MinitalkExample",
+		id:"MinitalkExampleV7",
 		channel:"example",
 		width:"100%",
 		height:"100%",
