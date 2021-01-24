15e7dd5c90d6f46fc8464a6d3f0563528adaad65
arzzcom <arzz@arzz.com>
2021-01-24 21:57:51 +0900

    첨부파일폴더 추가

---
+++ attachments/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 이 파일은 미니톡 클라이언트의 일부입니다. (https://www.minitalk.io)
+ *
+ * 폴더 접근 제한을 위한 Header 설정
+ * 
+ * @file /attachments/index.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 6.4.0
+ * @modified 2021. 1. 24.
+ */
+header('HTTP/1.1 404 Not Found');
+?>
