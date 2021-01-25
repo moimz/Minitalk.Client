f1ddb4ba8d52821980da209da5a7bc590849f65d
arzzcom <arzz@arzz.com>
2021-01-25 22:29:20 +0900

    관리자 로그아웃 추가

---
+++ process/logout.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 이 파일은 미니톡 클라이언트의 일부입니다. (https://www.minitalk.io)
+ *
+ * 미니톡 클라이언트 관리자 로그아웃을 처리한다.
+ * 
+ * @file /process/login.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 6.4.0
+ * @modified 2021. 1. 25.
+ */
+if (defined('__MINITALK__') == false) exit;
+
+unset($_SESSION['MINITALK_LOGGED']);
+$results->success = true;
+?>
