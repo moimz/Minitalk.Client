86ad5538d1797b864c1d1a448b9964389c1264fb
arzzcom <arzz@arzz.com>
2021-01-26 00:56:56 +0900

    호출시 브라우저 알림이 오도록 수정

--- scripts/widget.protocol.js
+++ scripts/widget.protocol.js
@@ -279,6 +279,7 @@ Minitalk.protocol = {
 	 */
 	call:function(data) {
 		Minitalk.ui.playSound("call");
+		Minitalk.ui.push(Minitalk.getText("action/called").replace("{nickname}",data.nickname));
 		Minitalk.ui.printSystemMessage("action",Minitalk.getText("action/called").replace("{nickname}",data.nickname));
 	},
 	/**
