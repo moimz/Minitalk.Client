b45970f452f56a3fef68f8c01ae158ea55338d6b
arzzcom <arzz@arzz.com>
2021-02-23 20:16:29 +0900

    채널삭제 추가채널삭제 추가

---
+++ process/@deleteChannel.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 이 파일은 미니톡 클라이언트의 일부입니다. (https://www.minitalk.io)
+ *
+ * 채널을 삭제한다.
+ * 
+ * @file /process/@deleteChannel.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 7.0.0
+ * @modified 2021. 2. 23.
+ */
+if (defined('__MINITALK__') == false) exit;
+
+$channels = Param('channels') ? json_decode(Request('channels')) : array();
+if (is_array($channels) == true && count($channels) > 0) {
+	$this->db()->delete($this->table->channel)->where('channel',$channels,'IN')->execute();
+}
+
+$results->success = true;
+?>
