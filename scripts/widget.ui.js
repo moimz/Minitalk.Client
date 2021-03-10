a5ba581370c42153ef424cca0acb57823141eaa9
arzzcom <arzz@arzz.com>
2021-03-10 13:09:25 +0900

    에러메시지가 제대로 표현되지 않는 문제수정

--- scripts/widget.ui.js
+++ scripts/widget.ui.js
@@ -1198,7 +1198,6 @@ Minitalk.ui = {
 	 */
 	printError:function(code,callback) {
 		Minitalk.ui.closeWindow();
-		Minitalk.socket.reconnectable = false;
 		
 		var $error = $("<div>").attr("data-role","error");
 		var $errorbox = $("<section>");
