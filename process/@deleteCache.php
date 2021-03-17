ab8f2894c5da96b4fb07802016dbd88f32fc5946
arzzcom <arzz@arzz.com>
2021-03-17 20:31:32 +0900

    캐시관리 추가

---
+++ process/@deleteCache.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 이 파일은 미니톡 클라이언트의 일부입니다. (https://www.minitalk.io)
+ *
+ * 캐시파일을 삭제한다.
+ * 
+ * @file /process/@deleteCache.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 6.4.1
+ * @modified 2021. 3. 17.
+ */
+if (defined('__MINITALK__') == false) exit;
+
+$names = Request('names') ? json_decode(Request('names')) : null;
+for ($i=0, $loop=count($names);$i<$loop;$i++) {
+	if (preg_match('/\.cache$/',$names[$i]) == false) continue;
+	
+	@unlink($this->getAttachmentPath().'/temp/'.$names[$i]);
+}
+
+$results->success = true;
+?>
