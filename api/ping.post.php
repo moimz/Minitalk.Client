5daf5e57412bb57edf624c133372160a7d191482
arzzcom <arzz@arzz.com>
2021-04-07 19:10:51 +0900

    채널 접속자수가 제대로 갱신되지 않는 버그수정

--- api/ping.post.php
+++ api/ping.post.php
@@ -8,34 +8,48 @@
  * @author Arzz (arzz@arzz.com)
  * @license MIT License
  * @version 6.4.2
- * @modified 2021. 3. 25.
+ * @modified 2021. 4. 7.
  */
 if (defined('__MINITALK__') == false) exit;
 
-$key = isset($headers['SECRET_KEY']) == true ? $headers['SECRET_KEY'] : null;
+$key = isset($headers['SECRET_KEY']) == true && strlen($headers['SECRET_KEY']) > 0 ? $headers['SECRET_KEY'] : null;
+$client_secret = isset($headers['CLIENT_SECRET']) == true && strlen($headers['CLIENT_SECRET']) > 0 ? $headers['CLIENT_SECRET'] : null;
 $domain = Request('domain');
 $time = Request('time');
 
-if (strlen($key) == 0 || $key != $_CONFIGS->key || strlen($domain) == 0) {
+if (($key == null && $client_secret == null) || strlen($domain) == 0) {
 	$data->success = false;
-	$data->message = 'MISSING PARAMTERS : SECRET_KEY OR DOMAIN';
+	$data->message = 'MISSING PARAMTERS : SECRET_KEY OR CLIENT_SECRET OR DOMAIN';
+	return;
+}
+
+if ($key != null && $key != $_CONFIGS->key) {
+	$data->success = false;
+	$data->message = 'INVALID PARAMETER : SECRET_KEY';
+	return;
+}
+
+if ($client_secret != null && $this->db()->select($this->table->server)->where('domain',$domain)->where('client_secret',$client_secret)->has() == false) {
+	$data->success = false;
+	$data->message = 'INVALID PARAMETER : CLIENT_SECRET';
 	return;
 }
 
 $user = Request('user') ? Request('user') : 0;
 $channel = Request('channel') ? Request('channel') : 0;
 $this->db()->update($this->table->server,array('status'=>'ONLINE','user'=>$user,'channel'=>$channel,'latest_update'=>time()))->where('domain',$domain)->execute();
+$this->db()->update($this->table->channel,array('user'=>0))->where('server',$server)->execute();
 
-$users = Request('users') ? Request('users') : array();
+$users = json_decode(Request('users')) ? json_decode(Request('users')) : array();
 foreach ($users as $room=>$user) {
 	$temp = explode('#',$room);
 	$channel = $temp[1];
 	
-	$this->db()->update($this->table->channel,array('user'=>$user))->where('channel',$channel)->execute();
+	$this->db()->update($this->table->channel,array('user'=>$user))->where('channel',$channel)->where('server',$domain)->execute();
 }
 
 $data->success = true;
 $data->client = round(array_sum(explode(' ',microtime())) * 1000);
-$data->server = $time;
-$data->timegap = sprintf('%0.3f',abs($data->client - $data->server) / 1000);
+$data->server = intval($time);
+$data->timegap = floatval(sprintf('%0.3f',abs($data->client - $data->server) / 1000));
 ?>
