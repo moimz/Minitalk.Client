05db2461afc74807697cb8606dce8e1ef899be4a
arzzcom <arzz@arzz.com>
2021-04-08 21:41:56 +0900

    클라이언트 버전 API 추가

---
+++ api/version.get.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 이 파일은 미니톡 클라이언트의 일부입니다. (https://www.minitalk.io)
+ *
+ * 미니톡 클라이언트의 버전을 반환한다.
+ * 
+ * @file /api/version.get.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 6.4.2
+ * @modified 2021. 4. 8.
+ */
+if (defined('__MINITALK__') == false) exit;
+
+$data->success = true;
+$data->version = __MINITALK_VERSION__;
+?>
