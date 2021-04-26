f028859f186fc23fb2975fa8b20bddb47aa3dbce
arzzcom <arzz@arzz.com>
2021-04-26 11:21:26 +0900

    rewrite 설정여부 확인 추가

---
+++ process/check.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 이 파일은 미니톡 클라이언트의 일부입니다. (https://www.minitalk.io)
+ *
+ * rewrite 설정여부를 확인한다.
+ * 
+ * @file /process/check.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 7.1.0
+ * @modified 2021. 4. 26.
+ */
+if (defined('__MINITALK__') == false) exit;
+
+$results->success = true;
+?>
