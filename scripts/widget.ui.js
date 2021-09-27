c334f3adba61c00b12351269a60b1e3cd2214634
arzzcom <arzz@arzz.com>
2021-09-27 14:45:12 +0900

    툴버튼에 레이어가 존재하는지 여부 추가

--- scripts/widget.ui.js
+++ scripts/widget.ui.js
@@ -7,7 +7,7 @@
  * @author Arzz (arzz@arzz.com)
  * @license MIT License
  * @version 6.5.2
- * @modified 2021. 8. 30.
+ * @modified 2021. 9. 27.
  */
 Minitalk.ui = {
 	domReady:false,
@@ -349,6 +349,12 @@ Minitalk.ui = {
 					
 					var $tool = $("<li>");
 					var $button = $("<button>").attr("type","button").attr("data-tool",tool);
+					
+					/**
+					 * 툴 레이어가 존재하는 경우
+					 */
+					if ($.inArray(tool,["color","emoticon"]) > -1) $button.attr("data-tool-layer","TRUE");
+					
 					$button.append($("<i>").addClass("icon"));
 					$button.append($("<span>").html(Minitalk.getText("tool/" + tool)));
 					$button.data("tool",tool);
@@ -374,6 +380,11 @@ Minitalk.ui = {
 				 */
 				var $tool = $("<li>");
 				var $button = $("<button>").attr("type","button").attr("data-tool",tool.name);
+				
+				/**
+				 * 툴 레이어가 존재하는 경우
+				 */
+				if (tool.layer === true) $button.attr("data-tool-layer","TRUE");
 				
 				var $icon = $("<i>");
 				if (tool.icon) $icon.css("backgroundImage","url(" + tool.icon + ")");
